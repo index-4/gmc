7fc17918028c6bf6c102841539a9549c297a1bf0
Korbinian Habereder <haberederkorbinian@googlemail.com>
2021-11-18 10:41:35 +0100

    feature test units :sparkles:
      - added t option which triggers tests before commiting / pushing
      - fixed optional command feature
    
    changelog-relevant

--- gmc_core.py
+++ gmc_core.py
@@ -7,26 +7,38 @@ from gmc_helper_classes import Task, Batch
 def parse_args() -> list:
     tasks = []
     arg_iter = iter(sys.argv[1:])
+    current_index = 1
     try:
         while arg := next(arg_iter):
             if task := gmc_args[arg]:
-                if task[0]:  # command must have args (e.g. commit message)
+                if task[0] == 1:  # command must have args (e.g. commit message)
                     try:
                         tasks.append(Task(task[2], task[1], next(arg_iter)))
+                        current_index += 1
                     except StopIteration:  # needed args were not given
                         if task[0] == 1:
                             raise Exception(f"Needed args for '{arg}' not given!")
                         else:
                             tasks.append(Task(task[2], task[1], "$d34d$"))
+                elif task[0] == 2: # command could have args
+                    try:
+                        if not gmc_args.is_key_known(sys.argv[current_index + 1]):
+                            tasks.append(Task(task[2], task[1], next(arg_iter)))
+                            current_index += 1
+                        else:
+                            tasks.append(Task(task[2], task[1]))
+                    except IndexError:
+                        tasks.append(Task(task[2], task[1]))
                 else:
                     tasks.append(Task(task[2], task[1]))
+            current_index += 1
     except StopIteration:  # no more args overall to parse
         pass
     except KeyError as failed_key:
         print(f"Given argument {failed_key} does not exist!")
         sys.exit()
     except Exception as ex:
-        print(ex)
+        print(f"UNKNOWN ERR: {ex}")
         sys.exit()
     # explicitly add magic add to tasks, if not single command or gmc only
     if len(sys.argv) != 2:
